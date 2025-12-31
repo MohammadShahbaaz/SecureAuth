bd8ae88a5a139946ce59c00f0bbcdd08e56458cb
MohammadShahbaaz <shahbaazmohd784@gmail.com>
2026-01-01 01:18:16 +0530

    Implemented secure user registration with password hashing

--- server/index.js
+++ server/index.js
@@ -1,19 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+
+const authRoutes = require("./routes/auth"); // 👈 import routes
 
 const app = express();
 
+/* ---------- MIDDLEWARE ---------- */
 app.use(cors());
 app.use(express.json());
 
+/* ---------- ROUTES ---------- */
+app.use("/api/auth", authRoutes); // 👈 ADD THIS HERE
+
+/* ---------- TEST ROUTE ---------- */
 app.get("/", (req, res) => {
   res.send("SecureAuth Backend Running");
 });
 
-const PORT = 5000;
+/* ---------- DATABASE ---------- */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
+/* ---------- SERVER ---------- */
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
