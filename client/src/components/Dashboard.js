b3af044732f20cee1bde3a2af0fa1c6bc92975f4
MohammadShahbaaz <shahbaazmohd784@gmail.com>
2026-01-01 05:51:10 +0530

    Added risk distribution chart to security dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,3 +1,11 @@
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+} from "recharts";
+
 import { useEffect, useState } from "react";
 
 function Dashboard({ token }) {
@@ -24,11 +32,48 @@ function Dashboard({ token }) {
       });
   }, [token]);
 
+  const riskData = [
+  {
+    name: "LOW",
+    value: history.filter((h) => h.riskLevel === "LOW").length,
+  },
+  {
+    name: "MEDIUM",
+    value: history.filter((h) => h.riskLevel === "MEDIUM").length,
+  },
+  {
+    name: "HIGH",
+    value: history.filter((h) => h.riskLevel === "HIGH").length,
+  },
+];
+
+const COLORS = ["#22c55e", "#facc15", "#ef4444"];
+
   return (
     <div style={{ padding: "30px" }}>
       <h2>🔐 Login Activity Dashboard</h2>
 
       {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <h3 style={{ marginTop: "30px" }}>⚠️ Login Risk Distribution</h3>
+
+<PieChart width={400} height={300}>
+  <Pie
+    data={riskData}
+    cx="50%"
+    cy="50%"
+    outerRadius={100}
+    dataKey="value"
+    label
+  >
+    {riskData.map((entry, index) => (
+      <Cell key={`cell-${index}`} fill={COLORS[index]} />
+    ))}
+  </Pie>
+  <Tooltip />
+  <Legend />
+</PieChart>
+
 
       <table border="1" cellPadding="8" style={{ marginTop: "20px" }}>
         <thead>
@@ -66,5 +111,6 @@ function Dashboard({ token }) {
     </div>
   );
 }
+
 
 export default Dashboard;
